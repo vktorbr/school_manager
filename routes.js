7668c8d64933524a4636fbefee17f13d314cac0c
Victor Martins <vms5@cin.ufpe.br>
2021-06-29 14:43:57 -0300

    criacao da pagina de criacao de professor e uso do Node fs para gravar localmente as infos

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+const teachers = require("./teachers");
 const express = require("express");
 const routes = express.Router();
 
@@ -8,6 +9,12 @@ routes.get("/", function(req, res){
 routes.get("/teachers", function(req, res){
     return res.render("teachers/index");
 })
+
+routes.get("/teachers/create", function(req, res){
+    return res.render("teachers/create");
+})
+
+routes.post("/teachers", teachers.post);
 
 routes.get("/students", function(req, res){
     return res.send("students");
